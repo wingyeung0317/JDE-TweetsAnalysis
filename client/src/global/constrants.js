bc0371b837f6259849eae84b083a0977872ed824
Wing <121206892+wingyeung0317@users.noreply.github.com>
2023-02-16 09:14:00 +0800

    Update constrants.js

--- client/src/global/constrants.js
+++ client/src/global/constrants.js
@@ -1,3 +1,3 @@
-export const API_HOST = 'http://14.136.34.157:5000'
+export const API_HOST = 'http://localhost:5000'
 export const CONNECT_FLASK = `${API_HOST}/connect`
 export const GET_TWEETS = `${API_HOST}/lsTweets`
